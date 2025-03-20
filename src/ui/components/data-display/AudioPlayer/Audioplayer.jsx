3e561a5b4c43e62f7fd908e1189360d41b5e12bb
Jhenrique44 <jo-henrique8@hotmail.com>
2025-03-20 16:17:29 -0300

    dinamic and embedded music player with a list of mock musics

--- src/ui/components/data-display/AudioPlayer/Audioplayer.jsx
+++ src/ui/components/data-display/AudioPlayer/Audioplayer.jsx
@@ -1,19 +1,37 @@
+/* eslint-disable no-unused-vars */
 import Timeline from "../../inputs/Timeline/Timeline";
-import {useState } from "react";
+import {useState, useRef } from "react";
 import styles from "./Audioplayer.module.css";
 
-export default function Audioplayer() {
+export default function Audioplayer(props) {
+    const [canPlay, setCanPlay] = useState(false);
     const [isPlaying, setIsPlaying] = useState(false);
     const [width, setWidth] = useState(0);
+    const audioRef = useRef(null);
+    const [duration, setDuration] = useState(1);
+    function onCanPlay() {
+        setDuration(audioRef.current.duration);
+        setCanPlay(true);   
+    }
+    function onEnded(){ 
+        setIsPlaying(false);
+        props?.onComplete();
+    }
     return (
         <div className={styles.playerContainer}>
             <div className={styles.buttonContainer}>
-                <button className={styles.playButton}>
+                <button className={styles.playButton} disable={!canPlay}>
                     {isPlaying ? '\u2759\u2759' : '\u25b8'}
                 </button>
             </div>
             <Timeline width={width} onChangeWidth={setWidth}/>
-            <audio controls className={styles.audio}></audio>
+            <audio 
+            ref={audioRef} 
+            src={props?.music?.url} 
+            onCanPlay={onCanPlay}
+            onEnded={onEnded}
+            controls 
+            className={styles.audio}></audio>
         </div>
     )
 }
--- src/ui/pages/index.jsx
+++ src/ui/pages/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import MusicList from "../components/data-display/MusicList/MusicList";
 // import Timeline from "../components/inputs/Timeline/Timeline";
 import Audioplayer from "../components/data-display/AudioPlayer/Audioplayer";
@@ -8,24 +8,27 @@ const musics = [
         id: 1,
         name: 'Musica 1',
         artist: 'Artista 1',
-        duration: 180
+        duration: 180,
+        url: ""
     },
     {
         id: 2,
         name: 'Musica 2',
         artist: 'Artista 2',
-        duration: 200
+        duration: 200,
+        url: ""
     },
     {
         id: 3,
         name: 'Musica 3',
         artist: 'Artista 3',
-        duration: 220
+        duration: 220,
+        url: ""
     }
 ]
 
 export default function Index() {
-    const [width, setWidth] = useState(20);
+    // const [width, setWidth] = useState(20);
     return (
         <>
             <MusicList
@@ -33,7 +36,7 @@ export default function Index() {
                 selectedMusic={musics[0]}
                 onSelect={()=> {}}
             />
-            <Audioplayer/>
+            <Audioplayer music={musics[0]}/>
         </>
     )
 }
